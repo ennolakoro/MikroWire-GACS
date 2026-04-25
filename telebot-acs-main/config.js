cdf152add5b0d79e8638b0ee0785703f75fa81ab
ennolakoro <eno@underc0de.us>
2026-04-25 21:28:34 +0800

    UI: Fixed missing bot files and added Modern Tabs UI

---
+++ telebot-acs-main/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // Konfigurasi untuk setiap server
+    servers: {
+        // Server 1
+        server1: {
+            name: "ALIJAYA-NET",
+            botToken: "1938127147:AAFMcxxxxxxxx",
+            adminIds: ["56785xxxxxx"],
+            customers: {},
+            genieacs: {
+                baseUrl: "http://192.168.8.xx:7557",
+                username: "admin",
+                password: "admin"
+            }
+        },
+        // Tambahkan server lain sesuai kebutuhan
+    }
+}; 
